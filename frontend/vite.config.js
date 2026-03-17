747377ae838a1c97fa42f4f9a6a3d06a21d954dd
Christian Zavala <crizaaron@gmail.com>
2026-03-16 22:36:36 -0600

    Trying to debug with a html response on 404 status

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,11 +10,18 @@ export default defineConfig(({ mode }) => ({
       name: 'copy-redirects',
       closeBundle() {
         // Copy _redirects to dist after build
-        const src = './public/_redirects'
-        const dest = './dist/_redirects'
-        if (fs.existsSync(src)) {
-          fs.copyFileSync(src, dest)
+        const srcRedirects = './public/_redirects'
+        const destRedirects = './dist/_redirects'
+        if (fs.existsSync(srcRedirects)) {
+          fs.copyFileSync(srcRedirects, destRedirects)
           console.log('Copied _redirects to dist')
+        }
+        // Copy test.html to dist after build
+        const srcTest = './public/test.html'
+        const destTest = './dist/test.html'
+        if (fs.existsSync(srcTest)) {
+          fs.copyFileSync(srcTest, destTest)
+          console.log('Copied test.html to dist')
         }
       }
     }
